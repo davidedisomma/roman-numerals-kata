529b611318d73713d57137ec0c3b2676949a3409
Davide Di Somma <davide.disomma@gmail.com>
2020-03-01 12:00:18 +0100

    [PHP] Convert digits when they correspond to a single roman numeral

--- php/src/RomanNumerals.php
+++ php/src/RomanNumerals.php
@@ -2,12 +2,30 @@
 
 namespace Kata;
 
+use PHPUnit\Util\Exception;
+
 final class RomanNumerals
 {
 
-    public static function fromDigit(int $int): string
+    public static function fromDigit(int $digit): string
     {
-
-        return 'I';
+        switch ($digit) {
+            case 1:
+                return 'I';
+            case 5:
+                return 'V';
+            case 10:
+                return 'X';
+            case 50:
+                return 'L';
+            case 100:
+                return 'C';
+            case 500:
+                return 'D';
+            case 1000:
+                return 'M';
+            default:
+                throw new Exception("Unexpected digit $digit");
+        }
     }
 }
--- php/test/RomanNumeralsTest.php
+++ php/test/RomanNumeralsTest.php
@@ -10,6 +10,12 @@ class RomanNumeralsTest extends TestCase
     public function testFromDigit(): void
     {
         $this->assertSame('I', RomanNumerals::fromDigit(1));
+        $this->assertSame('V', RomanNumerals::fromDigit(5));
+        $this->assertSame('X', RomanNumerals::fromDigit(10));
+        $this->assertSame('L', RomanNumerals::fromDigit(50));
+        $this->assertSame('C', RomanNumerals::fromDigit(100));
+        $this->assertSame('D', RomanNumerals::fromDigit(500));
+        $this->assertSame('M', RomanNumerals::fromDigit(1000));
     }
 
 
