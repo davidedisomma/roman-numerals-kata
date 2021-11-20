0073c2133185f12b68737d4bd8fddf476a7c76e6
Davide Di Somma <davide.disomma@gmail.com>
2021-11-20 14:44:57 +0100

    Power of ten rule owns next rule

--- php/src/ConversionPowerOfTenToRomanSymbol.php
+++ php/src/ConversionPowerOfTenToRomanSymbol.php
@@ -10,15 +10,33 @@ class ConversionPowerOfTenToRomanSymbol implements ConversionDigitToRomanNumeral
     private $powerOfTen;
     private $romanSymbol;
     private ConversionRuleFire $ruleFire;
+    private ?ConversionDigitToRomanNumeral $nextRule;
 
-    public function __construct(int $powerOfTen, string $romanSymbol, ConversionRuleFire $ruleFire)
+
+    public function __construct(int $powerOfTen, string $romanSymbol, ConversionRuleFire $ruleFire, ?ConversionDigitToRomanNumeral $nextRule = null)
     {
         $this->powerOfTen = $powerOfTen;
         $this->romanSymbol = $romanSymbol;
         $this->ruleFire = $ruleFire;
+        $this->nextRule = $nextRule;
     }
 
     public function convert(string $result, int $digit): array
+    {
+        list($result, $digit) = $this->fireRuleIfPossible($digit, $result);
+        if($this->nextRule !== null) {
+            return $this->nextRule->convert($result, $digit);
+        }
+
+        return array($result, $digit);
+    }
+
+    /**
+     * @param int $digit
+     * @param string $result
+     * @return array
+     */
+    private function fireRuleIfPossible(int $digit, string $result): array
     {
         if ($this->ruleFire->isFiredFor($digit)) {
             $howManyUnit = (int) ($digit / $this->powerOfTen);
--- php/src/RomanNumerals.php
+++ php/src/RomanNumerals.php
@@ -12,20 +12,21 @@ final class RomanNumerals
      */
     private static function rules(): array
     {
+        $until3Rule = new ConversionPowerOfTenToRomanSymbol(1, 'I', new BetweenTwoValues(0, 3), new NoConvertion());
+        $nineRule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(9, 'IX', new EqualToValue(9));
+        $between90And100Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(90, 'XC', new BetweenTwoValuesUpperBoundExcluded(90, 100));
+        $between900And1000Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(900, 'CM', new BetweenTwoValuesUpperBoundExcluded(900, 1000));
         return [
-            new ConversionPowerOfTenToRomanSymbol(1000, 'M', new BetweenTwoValues(1000, 3000)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(900, 'CM', new BetweenTwoValuesUpperBoundExcluded(900, 1000)),
+            new ConversionPowerOfTenToRomanSymbol(1000, 'M', new BetweenTwoValues(1000, 3000), $between900And1000Rule),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(500, 'D', new BetweenTwoValuesUpperBoundExcluded(500, 900)),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(400, 'CD', new BetweenTwoValuesUpperBoundExcluded(400, 500)),
-            new ConversionPowerOfTenToRomanSymbol(100, 'C', new BetweenTwoValuesUpperBoundExcluded(100, 400)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(90, 'XC', new BetweenTwoValuesUpperBoundExcluded(90, 100)),
+            new ConversionPowerOfTenToRomanSymbol(100, 'C', new BetweenTwoValuesUpperBoundExcluded(100, 400), $between90And100Rule),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(50, 'L', new BetweenTwoValuesUpperBoundExcluded(50, 90)),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(40, 'XL', new BetweenTwoValuesUpperBoundExcluded(40, 50)),
-            new ConversionPowerOfTenToRomanSymbol(10, 'X', new LessThanValue(40)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(9, 'IX', new EqualToValue(9)),
+            new ConversionPowerOfTenToRomanSymbol(10, 'X', new LessThanValue(40), $nineRule),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(5, 'V', new BetweenTwoValues(5, 8)),
             new ConversionIntermediateValuesBetweenTwoPowersOfTen(4, 'IV', new EqualToValue(4)),
-            new ConversionPowerOfTenToRomanSymbol(1, 'I', new BetweenTwoValues(0, 3))
+            $until3Rule
         ];
     }
 
