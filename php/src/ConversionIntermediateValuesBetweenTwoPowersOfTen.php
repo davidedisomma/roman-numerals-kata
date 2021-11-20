6d4259e3b0daa820189023c32a8d4f09248e0bbc
Davide Di Somma <davide.disomma@gmail.com>
2021-11-20 15:01:09 +0100

    Any rules owns next rule

--- php/src/ConversionIntermediateValuesBetweenTwoPowersOfTen.php
+++ php/src/ConversionIntermediateValuesBetweenTwoPowersOfTen.php
@@ -10,24 +10,34 @@ class ConversionIntermediateValuesBetweenTwoPowersOfTen implements ConversionDig
     private int $digitValue;
     private string $romanSymbol;
     private ConversionRuleFire $ruleFire;
+    private ConversionDigitToRomanNumeral $nextRule;
 
-    public function __construct(int $digitValue, string $romanSymbol, ConversionRuleFire $ruleFire)
+    public function __construct(int $digitValue, string $romanSymbol, ConversionRuleFire $ruleFire, ConversionDigitToRomanNumeral $nextRule)
     {
         $this->digitValue = $digitValue;
         $this->romanSymbol = $romanSymbol;
         $this->ruleFire = $ruleFire;
+        $this->nextRule = $nextRule;
     }
 
     public function convert(string $result, int $digit): array
     {
-        if($this->ruleFire != null) {
-            if($this->ruleFire->isFiredFor($digit)) {
-                $digit -= $this->digitValue;
-                return array($result.$this->romanSymbol, $digit);
-            }
-            return array($result, $digit);
+        list($result, $digit) = $this->fireRuleIfPossible($digit, $result);
+
+        return $this->nextRule->convert($result, $digit);
+    }
+
+    /**
+     * @param int $digit
+     * @param string $result
+     * @return array
+     */
+    private function fireRuleIfPossible(int $digit, string $result): array
+    {
+        if ($this->ruleFire->isFiredFor($digit)) {
+            $digit -= $this->digitValue;
+            return array($result.$this->romanSymbol, $digit);
         }
-        $digit -= $this->digitValue;
-        return array($result.$this->romanSymbol, $digit);
+        return array($result, $digit);
     }
 }
--- php/src/RomanNumerals.php
+++ php/src/RomanNumerals.php
@@ -8,26 +8,23 @@ use InvalidArgumentException;
 final class RomanNumerals
 {
     /**
-     * @return array<ConversionDigitToRomanNumeral>
+     * @return ConversionDigitToRomanNumeral
      */
-    private static function rules(): array
+    private static function rules(): ConversionDigitToRomanNumeral
     {
         $until3Rule = new ConversionPowerOfTenToRomanSymbol(1, 'I', new BetweenTwoValues(0, 3), new NoConvertion());
-        $nineRule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(9, 'IX', new EqualToValue(9));
-        $between90And100Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(90, 'XC', new BetweenTwoValuesUpperBoundExcluded(90, 100));
-        $between900And1000Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(900, 'CM', new BetweenTwoValuesUpperBoundExcluded(900, 1000));
-        return [
-            new ConversionPowerOfTenToRomanSymbol(1000, 'M', new BetweenTwoValues(1000, 3000), $between900And1000Rule),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(500, 'D', new BetweenTwoValuesUpperBoundExcluded(500, 900)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(400, 'CD', new BetweenTwoValuesUpperBoundExcluded(400, 500)),
-            new ConversionPowerOfTenToRomanSymbol(100, 'C', new BetweenTwoValuesUpperBoundExcluded(100, 400), $between90And100Rule),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(50, 'L', new BetweenTwoValuesUpperBoundExcluded(50, 90)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(40, 'XL', new BetweenTwoValuesUpperBoundExcluded(40, 50)),
-            new ConversionPowerOfTenToRomanSymbol(10, 'X', new LessThanValue(40), $nineRule),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(5, 'V', new BetweenTwoValues(5, 8)),
-            new ConversionIntermediateValuesBetweenTwoPowersOfTen(4, 'IV', new EqualToValue(4)),
-            $until3Rule
-        ];
+        $fourRule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(4, 'IV', new EqualToValue(4), $until3Rule);
+        $between5And8Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(5, 'V', new BetweenTwoValues(5, 8), $fourRule);
+        $nineRule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(9, 'IX', new EqualToValue(9), $between5And8Rule);
+        $lessThan40Rule = new ConversionPowerOfTenToRomanSymbol(10, 'X', new LessThanValue(40), $nineRule);
+        $between40And50Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(40, 'XL', new BetweenTwoValuesUpperBoundExcluded(40, 50), $lessThan40Rule);
+        $between50And90Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(50, 'L', new BetweenTwoValuesUpperBoundExcluded(50, 90), $between40And50Rule);
+        $between90And100Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(90, 'XC', new BetweenTwoValuesUpperBoundExcluded(90, 100), $between50And90Rule);
+        $between100And400Rule = new ConversionPowerOfTenToRomanSymbol(100, 'C', new BetweenTwoValuesUpperBoundExcluded(100, 400), $between90And100Rule);
+        $between400And500Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(400, 'CD', new BetweenTwoValuesUpperBoundExcluded(400, 500), $between100And400Rule);
+        $between500And900Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(500, 'D', new BetweenTwoValuesUpperBoundExcluded(500, 900), $between400And500Rule);
+        $between900And1000Rule = new ConversionIntermediateValuesBetweenTwoPowersOfTen(900, 'CM', new BetweenTwoValuesUpperBoundExcluded(900, 1000), $between500And900Rule);
+        return new ConversionPowerOfTenToRomanSymbol(1000, 'M', new BetweenTwoValues(1000, 3000), $between900And1000Rule);
     }
 
     public static function fromDigit(int $digit): string
@@ -36,9 +33,7 @@ final class RomanNumerals
             throw new InvalidArgumentException("Digit $digit is overflown");
         }
         $result = '';
-        foreach (self::rules() as $rule) {
-            list($result, $digit) = $rule->convert($result, $digit);
-        }
+        list($result) = self::rules()->convert($result, $digit);
         return $result;
     }
 }
